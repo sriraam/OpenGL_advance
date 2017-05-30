dd69d3c38df915a1e88750e3188e6ad09e79e31d
sriraam7447 <m.sriraam7447@gmail.com>
2017-05-30 11:33:15 +0530

    Added Mouse & Keyboard Events

--- OpenGL_frameBuffer/Source.cpp
+++ OpenGL_frameBuffer/Source.cpp
@@ -10,6 +10,13 @@
 // Camera Position
 float camX, camY, camZ;
 
+// Mouse Tracking Variables
+int startX, startY, tracking = 0;
+
+// Camera Spherical Coordinates
+float alpha = 40.0f, beta = 45.0f;
+float r = 5.25f;
+
 //shader shader_main;
 
 
@@ -42,9 +49,6 @@ void processKeys(unsigned char key, int xx, int yy)
 }
 
 void init() {
-
-	
-
 	// some GL settings
 	glEnable(GL_DEPTH_TEST);
 	//glEnable(GL_CULL_FACE);
@@ -52,6 +56,97 @@ void init() {
 	//glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	//Not neccessary to be written inside init(),usually written in main()
 
+}
+
+void processKeys(unsigned char key, int xx, int yy)
+{
+	switch (key) {
+
+	case 27:
+		glutLeaveMainLoop();
+		break;
+
+	case 'c':
+		printf("Camera Spherical Coordinates (%f, %f, %f)\n", alpha, beta, r);
+		break;
+
+	}
+
+	//  uncomment this if not using an idle func
+	glutPostRedisplay();
+}
+// Mouse Events
+//
+
+void processMouseButtons(int button, int state, int xx, int yy)
+{
+	// start tracking the mouse
+	if (state == GLUT_DOWN) {
+		startX = xx;
+		startY = yy;
+		if (button == GLUT_LEFT_BUTTON)
+			tracking = 1;
+		else if (button == GLUT_RIGHT_BUTTON)
+			tracking = 2;
+	}
+
+	//stop tracking the mouse
+	else if (state == GLUT_UP) {
+		if (tracking == 1) {
+			alpha -= (xx - startX);
+			beta += (yy - startY);
+		}
+		else if (tracking == 2) {
+			r += (yy - startY) * 0.01f;
+			if (r < 0.1f)
+				r = 0.1f;
+		}
+		tracking = 0;
+	}
+}
+
+// Track mouse motion while buttons are pressed
+
+void processMouseMotion(int xx, int yy)
+{
+
+	int deltaX, deltaY;
+	float alphaAux, betaAux;
+	float rAux;
+
+	deltaX = -xx + startX;
+	deltaY = yy - startY;
+
+	// left mouse button: move camera
+	if (tracking == 1) {
+
+
+		alphaAux = alpha + deltaX;
+		betaAux = beta + deltaY;
+
+		if (betaAux > 85.0f)
+			betaAux = 85.0f;
+		else if (betaAux < -85.0f)
+			betaAux = -85.0f;
+		rAux = r;
+	}
+	// right mouse button: zoom
+	else if (tracking == 2) {
+
+		alphaAux = alpha;
+		betaAux = beta;
+		rAux = r + (deltaY * 0.01f);
+		if (rAux < 0.1f)
+			rAux = 0.1f;
+	}
+
+
+	camX = rAux * sin(alphaAux * 3.14f / 180.0f) * cos(betaAux * 3.14f / 180.0f);
+	camZ = rAux * cos(alphaAux * 3.14f / 180.0f) * cos(betaAux * 3.14f / 180.0f);
+	camY = rAux *   						       sin(betaAux * 3.14f / 180.0f);
+
+	//  uncomment this if not using an idle func
+	glutPostRedisplay();
 }
 
 
@@ -76,11 +171,14 @@ int main(int argc, char** argv)
 	
 	init();
 	// glEnable(GL_DEPTH_TEST);
-	
+
+	//	Mouse and Keyboard Callbacks
+	glutKeyboardFunc(processKeys);
+	glutMouseFunc(processMouseButtons);
+	glutMotionFunc(processMouseMotion);
 
 	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
 	glutDisplayFunc(display1);
 
 	glutMainLoop();
-
 }
